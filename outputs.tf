49a4b39f6705a0835ab5d6c695389e82452dc6bc
Prathamesh Prakash Bhujade <129214303+prathamesh633@users.noreply.github.com>
2025-10-30 01:38:19 +0530

    Remove ssh_command output from outputs.tf
    Removed ssh_command output from outputs.tf.

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,3 @@ output "instance_id" {
 output "instance_public_ip" {
   value = aws_instance.web.public_ip
 }
-
-output "ssh_command" {
-  value = "ssh -i <your-private-key> ec2-user@${aws_instance.web.public_ip}  # or ssh -i <your-private-key> user1@${aws_instance.web.public_ip}"
-}
