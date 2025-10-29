39b3bf20a6c43d2ab8c4d0658cdadeea65ce9e17
Prathamesh Prakash Bhujade <129214303+prathamesh633@users.noreply.github.com>
2025-10-30 01:21:17 +0530

    Change default region and instance type in variables

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "t2.micro"
 }
 
 variable "key_name" {
