cf18f289d6a34497d9b6038ce7f0a90f33fda52c
Prathamesh Prakash Bhujade <129214303+prathamesh633@users.noreply.github.com>
2025-10-30 19:05:42 +0530

    Change default values for instance_type and allowed_cidr

--- variables.tf
+++ variables.tf
@@ -7,13 +7,13 @@ variable "region" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "t2.micro"
 }
 
 variable "allowed_cidr" {
   description = "CIDR allowed to SSH in (restrict this!)"
   type        = string
-  default     = "203.0.113.42/32"
+  default     = "10.0.0.0/16"
 }
 
 variable "name_prefix" {
